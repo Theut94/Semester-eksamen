7708dab214826c5228ea0d4f382a43b05b336253
anne507x <anne507x@easv365.dk>
2022-01-07 12:20:04 +0100

    Set up Main class to launch application window

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,8 +1,21 @@
 package gui;
 
-public class Main
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        primaryStage.setTitle("Movie Collection");
+        primaryStage.show();
     }
 }
