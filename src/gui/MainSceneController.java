95dfc5e13bf00d6622f768288a53d9eb3a2e6abe
m-eunbi <mariahansen.91@hotmail.com>
2022-01-12 10:58:37 +0100

    added connection between tableviews

--- src/gui/MainSceneController.java
+++ src/gui/MainSceneController.java
@@ -1,19 +1,25 @@
 package gui;
 
+import be.Category;
 import be.Movie;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainSceneController {
 
     public MenuButton menuButtonMovieOptions;
     public MenuButton menuButtonCategoryOptions;
     public TableView<Movie>tvMovies;
+    public TableColumn <Movie, String> tcMovieTitle;
+    public TableColumn <Movie, Float> tcRatingIMDB;
+    public TableColumn <Movie, Float> tcRatingPersonal;
+    public TableView<Category>tvCategories;
     private MainSceneModel mainSceneModel = new MainSceneModel();
 
     @FXML
@@ -36,8 +42,8 @@ public class MainSceneController {
         });
     }
 
-
     public void PlayMovie(ActionEvent actionEvent) {
+
     }
 
     public void ChangeRating(ActionEvent actionEvent) {
@@ -62,6 +68,26 @@ public class MainSceneController {
     }
 
     public void deleteMovie(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure you want to delete?", ButtonType.YES,ButtonType.NO);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.YES) {
+            Movie movie = tvMovies.getSelectionModel().getSelectedItem();
+            mainSceneModel.deleteMovie(movie);
+        }
+    }
+
+    public void selectCategory(MouseEvent mouseEvent) {
+        tcMovieTitle.setCellValueFactory(new PropertyValueFactory<Movie, String>("movieName"));
+        tcRatingIMDB.setCellValueFactory(new PropertyValueFactory<Movie, Float>("movieIMDBRating"));
+        tcRatingPersonal.setCellValueFactory(new PropertyValueFactory<Movie, Float>("moviePersonalRating"));
+        try {
+            if(!tvCategories.getSelectionModel().getSelectedItem().equals(tvCategories.getItems().get(0)))
+                tvMovies.setItems(mainSceneModel.getMoviesFromCategory(tvCategories.getSelectionModel().getSelectedItem()));
+            else
+                tvMovies.setItems(mainSceneModel.getAllMovies());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
